8f75f72712a19e4834bdfd0bc1f837cb9354fd8f
Nina <florinaseipp@gmail.com>
2021-12-25 21:45:18 +0100

    Entered the rel="noreferrer" into the footer link to react to warning.

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,11 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/Nina241o/a-react-weather-app">
+      <a
+        href="https://github.com/Nina241o/a-react-weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
         Open-Source code on Github
       </a>{" "}
       by Florina Seipp
