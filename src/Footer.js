fae66b41601c7353b1541309f6f48945e7fc9db2
Nina <florinaseipp@gmail.com>
2021-12-25 21:40:32 +0100

    Updated component Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/Nina241o/react-weather-app">
+      <a href="https://github.com/Nina241o/a-react-weather-app">
         Open-Source code on Github
       </a>{" "}
       by Florina Seipp
