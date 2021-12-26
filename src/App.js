ff16ed4cabd98e76ee77dd6b05c595c59fe7e26e
Nina <florinaseipp@gmail.com>
2021-12-26 20:21:22 +0100

    inserted Bootstrap in Terminal and index.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from "./Footer";
 export default function App() {
   return (
     <div className="App">
-      <div className="Container">
+      <div className="container">
         <div className="Card">
           <Weather />
           <br />
