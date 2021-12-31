d32aa8ebceed3aa75503ea730db02140101bcff5
Nina <florinaseipp@gmail.com>
2021-12-31 18:17:06 +0100

    refactored code after some warnings

--- src/UnitConversion.js
+++ src/UnitConversion.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import "./UnitConversion.css";
 
 export default function Temperature(props) {
-  const [unit, setUnit] = useState("celsius");
+  //const [unit, setUnit] = useState("celsius");
 
   //Decided not to use UnitConversion as long as not able to convert forecast temps as well (state management).
   //function showFahrenheit(event) {
@@ -11,21 +11,19 @@ export default function Temperature(props) {
   // setUnit("fahrenheit");
   //}
 
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
+  //function showCelsius(event) {
+  //  event.preventDefault();
+  //  setUnit("celsius");
+  //}
 
-  if (unit === "celsius") {
-    return (
-      <div className="UnitConversion">
-        <span className="currentTemperature">{Math.round(props.celsius)}</span>
-        <span className="units">°C</span>
-      </div>
-    );
-  } else {
-    return "Sorry, we cannot give you any Fahrenheit temperatures at this time.";
-  }
+  //if (unit === "celsius") {
+  return (
+    <div className="UnitConversion">
+      <span className="currentTemperature">{Math.round(props.celsius)}</span>
+      <span className="units">°C</span>
+    </div>
+  );
+  //  } else {
   //  let fahrenheit = (props.celsius * 9) / 5 + 32;
   //  return (
   //    <div className="UnitConversion">
